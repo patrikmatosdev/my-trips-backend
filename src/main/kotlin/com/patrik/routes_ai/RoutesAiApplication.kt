5c6d322e2febcda850625a8b394a0495cc1b9f57
Patrik Matos <patrikmatos.tecinfo@gmail.com>
2026-03-19 12:40:04 -0300

    initialize project

---
+++ src/main/kotlin/com/patrik/routes_ai/RoutesAiApplication.kt
@@ -0,0 +1,11 @@
+package com.patrik.routes_ai
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RoutesAiApplication
+
+fun main(args: Array<String>) {
+	runApplication<RoutesAiApplication>(*args)
+}
